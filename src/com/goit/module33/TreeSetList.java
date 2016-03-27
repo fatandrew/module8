aca9f4ebc655293b872326d8fda4022e55849267
Andrew Danylenko <wdanylenko@gmail.com>
2016-03-28 00:14:37 +0300

    Add

--- src/com/goit/module33/TreeSetList.java
+++ src/com/goit/module33/TreeSetList.java
@@ -7,18 +7,13 @@ import java.util.*;
  */
 public class TreeSetList {
     public static void main(String[] args) {
-        List<Guitar> guitars = new ArrayList<>();
-        guitars.add( new Guitar( "Bass", 1755 ) );
-        guitars.add( new Guitar( "Acoustic", 1955 ) );
-        guitars.add( new Guitar( "Hard", 13355 ) );
-        guitars.add( new Guitar( "Rock", 1755 ) );
 
-        SortedSet<String> stringSet = new TreeSet<String>(  );
-        stringSet.add( "ddd" );
-        stringSet.add( "ddd" );
-        stringSet.add( "ddd" );
-        stringSet.add( "ddd" );
-
+        TreeSet<String> stringSet = new TreeSet<String>(  );
+        stringSet.add( "Bach" );
+        stringSet.add( "Appolo" );
+        stringSet.add( "Wax" );
+        stringSet.add( "Candy" );
+        System.out.println(stringSet);
     }
 
 }
